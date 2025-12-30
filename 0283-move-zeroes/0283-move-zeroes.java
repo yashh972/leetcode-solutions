fe80c21753778567a25cf899a8fea3858de27610
Yash Jadhav <yashjadhav0207@gmail.com>
2025-12-30 18:05:49 +0530

    Time: 2 ms (88.89%), Space: 47.8 MB (53.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n =nums.length;
+        int j=-1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        if (j==-1) return;
+
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums,i,j);
+                j++;
+            }
+        }
+        
+    }
+    private void swap(int[] nums,int i,int j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+}
