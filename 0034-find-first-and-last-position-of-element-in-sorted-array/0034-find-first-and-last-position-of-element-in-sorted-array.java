740ce8c30a28fbc2c14405b5693a0417d0cde57e
Yash Jadhav <yashjadhav0207@gmail.com>
2026-03-19 22:14:48 +0530

    Time: 0 ms (100%), Space: 48.1 MB (83.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,54 @@
 class Solution {
-    private int lowerBound(int nums[],int target){
+    
+    private int lowerBound(int[] nums,int target){
         int n=nums.length;
         int low=0;
         int high=n-1;
+        int mid=0;
         int ans=n;
+
         while(low<=high){
-            int mid=low + (high-low)/2;
-            if(nums[mid] >=target){
+            mid=low +(high-low)/2;
+            if(nums[mid]>=target){
                 ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
-        }  
-          return ans;    
+            
+        }
+        return ans;
     }
 
-    private int upperBound(int nums[],int target){
+     private int upperBound(int[] nums,int target){
         int n=nums.length;
         int low=0;
         int high=n-1;
+        int mid=0;
         int ans=n;
+
         while(low<=high){
-            int mid=low + (high-low)/2;
-            if(nums[mid] >target){
+            mid=low +(high-low)/2;
+            if(nums[mid]>target){
                 ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
-        }  
-        return ans;    
+            
+        }
+        return ans;
     }
     public int[] searchRange(int[] nums, int target) {
         int n=nums.length;
-        int lb=lowerBound(nums,target);
         int ub=upperBound(nums,target)-1;
-
-        if(lb<=ub && lb< n && nums[lb]==target){
+        int lb=lowerBound(nums,target);
+        if(lb<=ub && ub<n && nums[lb]==target){
             return new int[]{lb,ub};
         }
         return new int[]{-1,-1};
+        
     }
 }
